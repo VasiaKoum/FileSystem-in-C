37fce5a2d23e93c3adc4f0657e90873e051e9b75
chariskms <chariskms@gmail.com>
2020-01-18 13:25:58 +0200

    change

--- structs.h
+++ structs.h
@@ -1,30 +1,25 @@
 #include <time.h>
-#define DATABLOCK_NUM 512
-#define FILENAME_SIZE 200
+#define DATABLOCK_NUM 10
 
 typedef struct{
-	unsigned int datablocks[DATABLOCK_NUM];
-} Datastream;
-
-typedef struct{
-	unsigned int nodeid;
-	unsigned int offset;
-	char *filename;
-	unsigned int size;
-	unsigned int type;
-	unsigned int parent_nodeid;
-	time_t creation_time;
-	time_t access_time;
-	time_t modification_time;
-	Datastream data;
+		unsigned int nodeid;
+		char *filename;
+		unsigned int size;
+		unsigned int type;
+		unsigned int parent-nodeid;
+		time_t creation_time;
+		time_t access_time;
+		time_t modification_time;
+		Datastream data;
 } MDS;
+
+typedef struct{
+		unsigned int datablocks[DATABLOCK_NUM];
+} Datastream;
 
 typedef struct{
 	unsigned int datablocks_size;
 	unsigned int metadata_size;
-	char path_root[FILENAME_SIZE];
-} Superblock;
+	char path_root[100];
 
-typedef struct{
-	char *array;
-} Bitmap;
+} Superblock;
